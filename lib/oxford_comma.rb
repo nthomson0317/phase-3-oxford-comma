74325efd4f20e8d8a0d17c465db9b59ab610293f
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-06 18:16:44 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+array = ["Tom", "Dick", "Harry"]
+def oxford_comma(array)
+return array.join(' and ') if array.size < 3
+array[-1] = "and " + array[-1]
+array.join(', ')
+# no_array.split
+
+    
 end
+
